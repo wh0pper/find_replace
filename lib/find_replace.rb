0da08db46aff996ae95081dc86c13128aa2c99ba
wh0pper <jaredclemmensen@gmail.com>
2018-02-07 14:45:51 -0800

    add ability to read and write to txt file

--- lib/find_replace.rb
+++ lib/find_replace.rb
@@ -1,17 +1,27 @@
 class Phrase
-  def initialize(input)
-    @input = input
+  def initialize(filename)
+    @file = filename
   end
 
   def find_replace(find, replace)
-    result = @input.gsub(find, replace)
+    in_file = File.open("#{@file}", 'r')
+    in_lines = in_file.readlines
+    out_file = File.open('output.txt', 'w')
+    in_lines.each do |line|
+      line = line.gsub(find, replace)
+      out_file.puts line
+    end
+    out_file.close
+    File.rename('test.txt', 'orig.txt')
+    File.rename('output.txt', 'test.txt')
   end
 end
 
-puts "Enter a phrase to work with:"
-phrase = Phrase.new(gets.chomp)
+puts "Enter the file name to work with:"
+input = gets.chomp
+phrase = Phrase.new(input)
 puts "Enter the word to be replaced:"
 find = gets.chomp
 puts "Enter the replacement word:"
 replace = gets.chomp
-puts(phrase.find_replace(find, replace))
+phrase.find_replace(find, replace)
