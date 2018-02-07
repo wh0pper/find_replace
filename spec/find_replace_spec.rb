46cd75faa3843559323e81d61505b51896a4b11d
wh0pper <jaredclemmensen@gmail.com>
2018-02-07 11:02:18 -0800

    add further test specs

--- spec/find_replace_spec.rb
+++ spec/find_replace_spec.rb
@@ -6,4 +6,9 @@ describe ('#find_replace') do
     phrase = Phrase.new('Hello world')
     expect(phrase.find_replace('world', 'universe').eql?('Hello universe'))
   end
+
+  it "finds substring and replaces all instances" do
+    phrase = Phrase.new('testing intesting test')
+    expect(phrase.find_replace('test', 'pass').eql?('passing inpassing pass'))
+  end
 end
